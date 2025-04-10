122be6096045b89c8cad307f89001f1b70afd296
Kate Bakhtina <almazniegori@gmail.com>
2025-04-11 00:07:46 +0300

    testing by testng

---
+++ src/test/java/ru/netology/automationBasics/CashbackHackServiceTest.java
@@ -0,0 +1,27 @@
+package ru.netology.automationBasics;
+
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void remainTest() {
+
+        SoftAssert softAssert = new SoftAssert();
+
+        CashbackHackService service = new CashbackHackService();
+        int[] arrayPayment = {0, 1, 999, 1000, 1001, 2000};
+        int[] arrayExpected = {1000, 999, 1, 0, 999, 0};
+
+        for (int i = 0; i < arrayPayment.length; i++) {
+            int payment = arrayPayment[i];
+            int actual = service.remain(payment);
+            int expected = arrayExpected[i];
+            softAssert.assertEquals(actual, expected, "Платеж: " + payment);
+        }
+        softAssert.assertAll();
+     }
+}
