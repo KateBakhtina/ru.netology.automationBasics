493aeef3a4fa0358e62e3457bfadcba7e9545b94
Kate Bakhtina <almazniegori@gmail.com>
2025-04-12 19:07:54 +0300

    added junit4

--- src/test/java/ru/netology/automationBasics/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/automationBasics/CashbackHackServiceTest.java
@@ -1,27 +1,39 @@
 package ru.netology.automationBasics;
 
 
-import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
+import org.junit.Test;
+import org.junit.Assert;
 
-import static org.testng.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
     @Test
     public void remainTest() {
 
-        SoftAssert softAssert = new SoftAssert();
-
         CashbackHackService service = new CashbackHackService();
-        int[] arrayPayment = {0, 1, 999, 1000, 1001, 2000};
-        int[] arrayExpected = {1000, 999, 1, 0, 999, 0};
 
-        for (int i = 0; i < arrayPayment.length; i++) {
-            int payment = arrayPayment[i];
-            int actual = service.remain(payment);
-            int expected = arrayExpected[i];
-            softAssert.assertEquals(actual, expected, "Платеж: " + payment);
-        }
-        softAssert.assertAll();
+        int amount1 = 0;
+        int expected1 = 1000;
+        int actual1 = service.remain(amount1);
+        Assert.assertEquals(expected1, actual1);
+
+        int amount2 = 1;
+        int expected2 = 999;
+        int actual2 = service.remain(1);
+        Assert.assertEquals(expected2, actual2);
+
+        int amount3 = 999;
+        int expected3 = 1;
+        int actual3 = service.remain(999);
+        Assert.assertEquals(expected3, actual3);
+
+        int amount4 = 1000;
+        int expected4 = 0;
+        int actual4 = service.remain(1000);
+        Assert.assertEquals(expected4, actual4);
+
+        int amount5 = 1001;
+        int expected5 = 999;
+        int actual5 = service.remain(1001);
+        Assert.assertEquals(expected5, actual5);
      }
 }
